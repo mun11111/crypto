b3ede3b14769a85f7dd3ea6f389e6bde70311931
mun11111 <myunggimoon@gmail.com>
2022-03-04 17:45:57 +0900

    4.2 end

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,12 +1,36 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-const Container = styled.div ``;
+const Container = styled.div `
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
 
 const Header = styled.header ``;
 
 const CoinsList = styled.ul``;
 
-const Coin = styled.li ``;
+const Coin = styled.li `
+  background-color: white;
+  color:${props => props.theme.bgColor};
+  /* padding: 20px; */
+  border-radius: 15px;
+  margin-bottom: 10px;
+
+  a{
+    padding: 20px;
+    transition: color 0.2s ease-in;
+    
+    display: block;  /* 박스전체를 클릭 가능하게 */
+  }
+
+  &:hover {
+    a {
+      color:${(props) => props.theme.accentColor}
+    }
+  }
+`;
 
 
 const Title = styled.h1`
@@ -52,7 +76,9 @@ function Coins() {
       </Header>
       <CoinsList>
         {coins.map ((coin) => (
-          <Coin key={coin.id}> {coin.name}</Coin>
+          <Coin key={coin.id}> 
+            <Link to={`/${coin.id}`}> {coin.name} &rarr; </Link>
+          </Coin>
         ))} 
      </CoinsList>
     </Container>
